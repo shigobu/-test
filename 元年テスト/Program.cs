3c25f3eaa5d00775d9682f8610b425ddc5def319
shigobu <shigobu@shigobu.com>
2019-03-06 13:04:43 +0900

    プロジェクト ファイルを追加します。

---
+++ 元年テスト/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 元年テスト
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
