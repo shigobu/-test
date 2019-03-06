764e9c797a5839a87eb2c15bf5945c9f6bededb4
shigobu <shigobu@shigobu.com>
2019-03-06 14:02:32 +0900

    細かい修正

--- 元年テスト/Program.cs
+++ 元年テスト/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace 元年テスト
     {
         static void Main(string[] args)
         {
+            DateTime date = new DateTime(1989, 8, 18);
+            var cal = new JapaneseCalendar();
+            var jaJP = new CultureInfo("ja-JP", false);
+            jaJP.DateTimeFormat.Calendar = cal;
+            Console.WriteLine(date.ToString("ggy年MM月dd日", jaJP));
+            Console.WriteLine($"{date:ggy年MM月d日}");
+            Console.WriteLine(date.ToString("ggy'年'MM月dd日", jaJP));
+            Console.WriteLine($"{date:ggy'年'MM月d日}");
+            Console.Read();
         }
     }
 }
